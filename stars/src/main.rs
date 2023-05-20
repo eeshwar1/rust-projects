12cc7d486ef2834a930b17439ee6e37220520b1b
Venky Venkatakrishnan <venkyul@gmail.com>
2023-05-19 21:09:47 -0500

    Add files via upload
    Added command line arguments and logic to print reverse rows

--- stars/src/main.rs
+++ stars/src/main.rs
@@ -1,35 +1,41 @@
-use std::io;
+use std::env;
 
 fn main() {
     
-    println!("Please enter the number of rows: ");
-    
-    let mut rows = String::new();
-    
-    io::stdin()
-        .read_line(&mut rows)
-        .expect("Failed to read line");
-    
-    let rows: u32 = rows.trim().parse().expect("Please type a number!");
+    let args: Vec<String> = env::args().collect();
+     
+    let rows: u32 = args[1].trim().parse().expect("Please type a number");
     
     for number in 0..rows {
         
-        let mut num_stars:u32 = 0;
-        let mut num_spaces:u32 = 0;
-        let row_stars = 2 * number + 1;
-        
-        let mut text=String::from("");
-        
-        while num_spaces < rows - number {
-            
-            text.push_str("  ");
-            num_spaces += 1;
-        }
-        while num_stars < row_stars {
-            
-            text.push_str("* ");
-            num_stars += 1;
-        }
-        println!("{}",text);
+       print_row(rows, number)
     }
+    
+    for number in (0..rows-1).rev() {
+        
+        print_row(rows, number)
+       
+    }
+}
+
+fn print_row(max_rows: u32, row_number: u32) {
+    
+    let mut num_stars:u32 = 0;
+    let mut num_spaces:u32 = 0;
+    let row_stars = 2 * row_number + 1;
+    
+    let mut text=String::from("");
+    
+    while num_spaces < max_rows - row_number {
+        
+        text.push_str("  ");
+        num_spaces += 1;
+    }
+    while num_stars < row_stars {
+        
+        text.push_str("* ");
+        num_stars += 1;
+    }
+    println!("{}",text);
+    
 }
