41bb5ff9ae2337d6d4f20d6ccfde403b54ebffcd
Venky Venkatakrishnan <venkyul@gmail.com>
2023-06-03 20:32:46 -0500

    Delete stars/target directory

--- stars/target/debug/deps/stars-9b2326577d2aa0be.d
+++
@@ -1,5 +0,0 @@
-/media/easwar/data/developer/rust/projects/stars/target/debug/deps/stars-9b2326577d2aa0be: src/main.rs
-
-/media/easwar/data/developer/rust/projects/stars/target/debug/deps/stars-9b2326577d2aa0be.d: src/main.rs
-
-src/main.rs:
--- stars/target/debug/stars.d
+++
@@ -1,1 +0,0 @@
-/media/easwar/data/developer/rust/projects/stars/target/debug/stars: /media/easwar/data/developer/rust/projects/stars/src/main.rs
